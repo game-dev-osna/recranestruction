691e735e301c121b12b463953d36ebcbbe8f0f27
Malte Weiß <moolt93@gmx.de>
2020-01-31 22:02:41 +0100

    Adds high poly crane

---
+++ Assets/Scripts/CraneControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneControl : MonoBehaviour
+{
+    [SerializeField] private InputAction _test;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
