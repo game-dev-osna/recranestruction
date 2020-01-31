6ea064e0057767d6f30589b78207ca877dde2332
Christoph Rohmann <christoph.rohmann@gmail.com>
2020-01-31 22:43:03 +0100

    Crane Rotation 🐱
    cancer

--- Assets/Scripts/CraneControl.cs
+++ Assets/Scripts/CraneControl.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CraneControl : MonoBehaviour
 {
-    [SerializeField] private InputAction _test;
+    [SerializeField] private InputAction _rotate;
+    [SerializeField] private float _rotationSpeed;
     
-    void Start()
+    void OnEnable()
     {
-        
+        _rotate.Enable();
+    }
+
+    void OnDisable()
+    {
+        _rotate.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var value = _rotate.ReadValue<float>();
+        transform.localEulerAngles += new Vector3(0,value,0) * Time.deltaTime * _rotationSpeed;
     }
 }
