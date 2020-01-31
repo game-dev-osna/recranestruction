b72e7fb9f451156e1d69f5ae281bbffd416a94fb
Erik Wittkorn <erik.wittkorn@googlemail.com>
2020-02-01 00:56:42 +0100

    Updated Goo Prefab 🍏

---
+++ Assets/Scripts/BuildingBlockScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingBlockType { Type0, Type1, Type2};
+
+public class BuildingBlockScript : MonoBehaviour
+{
+
+    public bool isGhost;
+
+    public BuildingBlockType targetTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
