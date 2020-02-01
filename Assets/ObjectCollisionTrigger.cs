3e2b3b394cf1edc91f3fef4c44dd754815856068
Malte Weiß <moolt93@gmx.de>
2020-02-01 18:49:51 +0100

    water level now somewhat playable

---
+++ Assets/ObjectCollisionTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public enum WaterLevelTriggerType { Lost, Moved}
+
+public class ObjectCollisionTrigger : MonoBehaviour
+{
+    public WaterLevelTriggerType _type;
+    public WaterLevelController _controller;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Draggable"))
+        {
+            return;
+        }
+
+        if (other.transform.parent != null)
+        {
+            return;
+        }
+
+        if (_type == WaterLevelTriggerType.Lost)
+        {
+            _controller.OnObjectWasLost(other.transform);
+        }
+
+        if (_type == WaterLevelTriggerType.Moved)
+        {
+            _controller.OnObjectWasMoved(other.transform);
+        }
+    }
+}
---
+++ Assets/WaterLevelController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterLevelController : MonoBehaviour
+{
+    public Text _lostText;
+    public Text _movedText;
+
+    private int _instanceCount;
+    private int _moved;
+    private int _lost;
+
+    private void Start()
+    {
+        UpdateTexts();
+    }
+
+    public void OnObjectWasLost(Transform other)
+    {
+        _lost++;
+        UpdateTexts();
+        Destroy(other.gameObject);
+    }
+
+    public void OnObjectWasMoved(Transform other)
+    {
+        _moved++;
+        UpdateTexts();
+        Destroy(other.gameObject);
+    }
+
+    private void UpdateTexts()
+    {
+        _movedText.text = "Moved: " + _moved;
+        _lostText.text = "Lost: " + _lost;
+    }
+}
