313116378498c9c8721bea568a77b898bfd324af
Malte Weiß <moolt93@gmx.de>
2020-02-01 11:31:58 +0100

    Length of rope can be modified

--- Assets/Scripts/CraneControl.cs
+++ Assets/Scripts/CraneControl.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CraneControl : MonoBehaviour
@@ -11,26 +9,48 @@ public class CraneControl : MonoBehaviour
     [SerializeField] private float _friction;
     [SerializeField] private float _maxSpeed;
 
+    [SerializeField] private InputAction _ropeLengthInput;
+    [SerializeField] private float _ropeSpeed;
+    [SerializeField] private float _minLength = 0;
+    [SerializeField] private float _maxLength = 0.35f;
+    [SerializeField] private RopeControllerRealisticNoSpring _ropeController;
+
     private float speed;
 
     void OnEnable()
     {
         _rotate.Enable();
+        _ropeLengthInput.Enable();
     }
 
     void OnDisable()
     {
         _rotate.Disable();
+        _ropeLengthInput.Disable();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        CalculateRotation();
+        CalculcateRopeLength();
+    }
+
+    private void CalculateRotation()
     {
         float accel = _acceleration * _rotate.ReadValue<float>();
         float wouldBeSpeed = (speed + Time.deltaTime * accel) * _friction;
         speed = Mathf.Clamp(wouldBeSpeed, -_maxSpeed, _maxSpeed);
         // float wouldBeSpeed = (speed + Time.deltaTime * accel) - Mathf.Sign(speed) * _friction * Time.deltaTime;
         // speed = speed < 0 ? Mathf.Clamp(wouldBeSpeed, -_maxSpeed, 0) : Mathf.Clamp(wouldBeSpeed, 0, _maxSpeed);
-        _mainPart.localEulerAngles += Time.deltaTime * new Vector3(0,speed,0);
+        _mainPart.localEulerAngles += Time.deltaTime * new Vector3(0, speed, 0);
+    }
+
+    private void CalculcateRopeLength()
+    {
+        var lengthChange = _ropeLengthInput.ReadValue<float>();
+        Debug.Log(lengthChange);
+        var newLength = (lengthChange * _ropeSpeed * Time.deltaTime) + _ropeController.ropeSectionLength;
+        _ropeController.ropeSectionLength = Mathf.Clamp(newLength, _minLength, _maxLength);
     }
 }
