280e563f714c5f0f5c0619546ffca88313163a4d
Malte Weiß <moolt93@gmx.de>
2020-02-01 21:43:09 +0100

    pushes everything

---
+++ Assets/CarGameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CarGameController : MonoBehaviour
+{
+    public JurassicCar _car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartCar()
+    {
+        _car.MakeTheCarMoveIntoTheDirectionItIsCurrentlyLookingAtWithAPredefinedVelocityPlease(13.37f);
+    }
+
+    public void ResetCar()
+    {
+        _car.ResetCar();
+    }
+
+    public void OnWin()
+    {
+
+    }
+}
---
+++ Assets/JurassicCar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JurassicCar : MonoBehaviour
+{
+    public Transform exploder;
+    public float _explosionEngineForce = 0.5f;
+    private Rigidbody _rigidbody;
+    // Start is called before the first frame update
+
+    private Vector3 _startPosition;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.AddForce(Vector3.right * _explosionEngineForce);
+    }
+
+    public void MakeTheCarMoveIntoTheDirectionItIsCurrentlyLookingAtWithAPredefinedVelocityPlease(float force)
+    {
+        _explosionEngineForce = force;
+    }
+
+    public void ResetCar()
+    {
+        transform.position = _startPosition;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        _explosionEngineForce = 0f;
+    }
+}
