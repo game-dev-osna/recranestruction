592956c616565c7baeaf0e16f216564e2581dc52
Niels Meyering <niels@nilux.space>
2020-02-01 11:24:13 +0100

    extend tower height, add alien to crane, add sparks and brake sound

--- Assets/Scripts/CraneControl.cs
+++ Assets/Scripts/CraneControl.cs
@@ -10,6 +10,12 @@ public class CraneControl : MonoBehaviour
     [SerializeField] private Transform _mainPart;
     [SerializeField] private float _friction;
     [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _fastEnoughForSparks;
+    [SerializeField] private float _brakeBoost;
+    [SerializeField] private ParticleSystem _sparks;
+    [Space]
+    [SerializeField] private AudioClip _brakeSound;
+    [SerializeField] private AudioSource _audioSource;
 
     private float speed;
 
@@ -26,11 +32,27 @@ public class CraneControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float accel = _acceleration * _rotate.ReadValue<float>();
+        float input = _rotate.ReadValue<float>();
+        float accel = _acceleration * input;
+        // actively steering against rotation
+        if (Mathf.Abs(input) > .5f && input < 0 != speed < 0)
+        {
+            accel *= _brakeBoost;
+            if (Mathf.Abs(speed) > _fastEnoughForSparks)
+            {
+                _sparks.Play();
+                if (!_audioSource.isPlaying)
+                    _audioSource.PlayOneShot(_brakeSound);
+            }
+            else
+            {
+                _sparks.Stop();
+            }
+        }
         float wouldBeSpeed = (speed + Time.deltaTime * accel) * _friction;
         speed = Mathf.Clamp(wouldBeSpeed, -_maxSpeed, _maxSpeed);
         // float wouldBeSpeed = (speed + Time.deltaTime * accel) - Mathf.Sign(speed) * _friction * Time.deltaTime;
         // speed = speed < 0 ? Mathf.Clamp(wouldBeSpeed, -_maxSpeed, 0) : Mathf.Clamp(wouldBeSpeed, 0, _maxSpeed);
-        _mainPart.localEulerAngles += Time.deltaTime * new Vector3(0,speed,0);
+        _mainPart.localEulerAngles += Time.deltaTime * new Vector3(0, speed, 0);
     }
 }
