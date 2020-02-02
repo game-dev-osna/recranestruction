631f177fddd6cfdcdbe8b9cec7f2f4213dab0041
Erik Wittkorn <erik.wittkorn@googlemail.com>
2020-02-02 14:38:37 +0100

    Menu stuff

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class GameMaster : MonoBehaviour
+{
+
+    static string loadedScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("Clicked Start");
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/Menu/CanvasScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour
+{
+
+    float sWidth;
+    float sHeight;
+
+    RectTransform canvasTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sWidth = Screen.width;
+        sHeight = Screen.height;
+
+        canvasTransform = GetComponent<RectTransform>();
+
+        //Debug.Log($"Width: {sWidth} Height: {sHeight}");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
