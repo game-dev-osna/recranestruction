08a1e5b10d69016e983d0af6f9bbf1ac512fb23a
Malte Weiß <moolt93@gmx.de>
2020-02-02 16:04:00 +0100

    level success

--- Assets/FireLevelGameManager.cs
+++ Assets/FireLevelGameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class FireLevelGameManager: MonoBehaviour
 {
+    public GameObject _successScreen;
     public Text _remainingText;
     public int fires;
 
@@ -15,6 +16,11 @@ public class FireLevelGameManager: MonoBehaviour
     {
         fires--;
         UpdateTexts();
+
+        if(fires == 0)
+        {
+            Instantiate(_successScreen);
+        }
     }
 
     private void UpdateTexts()
