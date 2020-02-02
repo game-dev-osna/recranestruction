e1e2fa18149f2c6cc44160f86c1d2e87f2b69bee
Christoph Rohmann <christoph.rohmann@gmail.com>
2020-02-02 16:06:07 +0100

    thruster sound

--- Assets/Thrusters.cs
+++ Assets/Thrusters.cs
@@ -10,14 +10,45 @@ public class Thrusters : MonoBehaviour
     public ParticleSystem back;
 
     public GooController gooController;
+    public AudioSource audioA;
+    public AudioSource audioB;
 
+
+    private void Start()
+    {
+        forward.Stop();
+        back.Stop();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(gooController.thrust.x > 0) right.Play(); else right.Stop();
-        if(gooController.thrust.x < 0) left.Play(); else left.Stop();
-        if(gooController.thrust.y > 0) forward.Play(); else forward.Stop();
-        if(gooController.thrust.y < 0) back.Play(); else back.Stop();
+        if (gooController.thrust.x > 0)
+        {
+            if(!audioA.isPlaying)
+                audioA.Play();
+            right.Play();
+        }
+        else
+        {
+            if (audioA.isPlaying)
+                audioA.Stop();
+            right.Stop(); 
+        }
+        if (gooController.thrust.x < 0)
+        {
+            if (!audioB.isPlaying)
+                audioB.Play();
+            left.Play();
+        }
+        else
+        {
+            if (audioB.isPlaying)
+                audioB.Stop();
+            left.Stop(); 
+        }
+        //if (gooController.thrust.y > 0) forward.Play(); else forward.Stop();
+        //if (gooController.thrust.y < 0) back.Play(); else back.Stop();
+
     }
 }
