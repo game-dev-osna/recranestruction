1f29f7df44802051937da11017bc2adb326773ad
Malte Weiß <moolt93@gmx.de>
2020-02-02 15:37:19 +0100

    adds fire level

---
+++ Assets/FireLevelGameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FireLevelGameManager: MonoBehaviour
+{
+    public Text _remainingText;
+    public int fires;
+
+    private void Start()
+    {
+        UpdateTexts();
+    }
+
+    public void FireRemoved()
+    {
+        fires--;
+        UpdateTexts();
+    }
+
+    private void UpdateTexts()
+    {
+        _remainingText.text = "Remaining: " + fires;
+    }
+}
