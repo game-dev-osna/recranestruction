186af66ebf233491ba25da9ba38890432099b04a
Christoph Rohmann <christoph.rohmann@gmail.com>
2020-02-02 16:11:21 +0100

    fix thrust

--- Assets/Thrusters.cs
+++ Assets/Thrusters.cs
@@ -27,25 +27,29 @@ public class Thrusters : MonoBehaviour
         {
             if(!audioA.isPlaying)
                 audioA.Play();
-            right.Play();
+            if(!right.isPlaying)
+                right.Play();
         }
         else
         {
             if (audioA.isPlaying)
                 audioA.Stop();
-            right.Stop(); 
+            if (right.isPlaying)
+                right.Stop(); 
         }
         if (gooController.thrust.x < 0)
         {
             if (!audioB.isPlaying)
                 audioB.Play();
-            left.Play();
+            if (!left.isPlaying)
+                left.Play();
         }
         else
         {
             if (audioB.isPlaying)
                 audioB.Stop();
-            left.Stop(); 
+            if (left.isPlaying)
+                left.Stop(); 
         }
         //if (gooController.thrust.y > 0) forward.Play(); else forward.Stop();
         //if (gooController.thrust.y < 0) back.Play(); else back.Stop();
